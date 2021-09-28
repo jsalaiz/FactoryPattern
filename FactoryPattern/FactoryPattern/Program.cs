963699d2df37f2829db5bf0c996527239a71ab4a
jsalaiz <jonathan.salaiz@gmail.com>
2021-09-27 20:56:13 -0400

    Completed the assignement

---
+++ FactoryPattern/FactoryPattern/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FactoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a the number 2 to make a motorcycle or the number 4 to make a car");
+            string userInput = Console.ReadLine();
+            IVehicle userChoice = VehicleFactory.GetVehicle(userInput);
+            userChoice.Drive();
+        }
+    }
+}
---
+++ FactoryPattern/FactoryPattern/VehicleFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public static class VehicleFactory
+    {
+        public static IVehicle GetVehicle(string userInput)
+        {
+            if(userInput == "4") { return new Car(); }
+            else if (userInput == "2") { return new Motorcycle(); }
+            else { return new Car(); }
+        }
+    }
+}
